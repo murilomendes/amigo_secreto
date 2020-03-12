e07cb99c05c4741196a915550cd33dfc1e6ac5f7
Murilo Mendes <mmendes090@live.com>
2020-03-12 17:08:15 -0300

    add tests for campaign controller

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -1,47 +1,173 @@
 require 'rails_helper'
 
 RSpec.describe CampaignsController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
+  before(:each) do 
+    request.env["HTTP_ACCEPT"] = 'application/json'
+
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryBot.create(:user)
+    sign_in @current_user
   end
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe "GET #index" do 
+    it "returns http success" do 
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "GET #show" do
+    context "campaign exists" do
+      context "User is the owner of the campaign" do
+        it "returns success" do
+          campaign = create(:campaign, user: @current_user)
+          get :show, params: {id: campaign.id}
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "User is not the owner of the campaign" do 
+        it "Redirects to root" do 
+          campaign = create(:campaign)
+          get :show, params: {id: campaign.id}
+
+          expect(response).to redirect_to('/')
+        end
+      end
+    end
+
+    context "campaign doesn`t exists" do
+      it "redirect to root" do 
+        get :show, params: {id: 0}
+        expect(response).to redirect_to('/')
+      end
     end
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do 
+    before(:each) do 
+      @campaign_attributes = attributes_for(:campaign, user: @current_user)
+      post :create, params: {campaign: @campaign_attributes}
+    end
+
+    it "redirects to a new campaign" do 
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to("/campaigns/#{Campaign.last.id}")
+    end
+
+    it "create campaign with initial params" do 
+      expect(Campaign.last.user).to eql(@current_user)
+      expect(Campaign.last.title).to eql("Nova Campanha")
+      expect(Campaign.last.description).to eql("Descreva sua campanha...")
+      expect(Campaign.last.status).to eql("pending")
+    end
+
+    it "create campaign with owner associated as a member" do
+      expect(Campaign.last.members.last.name).to eql(@current_user.name)
+      expect(Campaign.last.members.last.email).to eql(@current_user.email)
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe "DELETE #destroy" do
+    before(:each) do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
+
+    context "User is the Campaign`s owner" do 
+      it "return http success" do
+        campaign = create(:campaign, user: @current_user)
+        delete :destroy, params: {id: campaign.id}
+        expect(response).to have_http_status(:success)
+      end
+
+      context "User isn`t the campaign owner" do 
+        it "returns http forbidden" do
+          campaign = create(:campaign)
+          delete :destroy, params: {id: campaign.id}
+          expect(response).to have_http_status(:forbidden)
+        end
+      end
     end
   end
 
-  describe "GET #raffle" do
-    it "returns http success" do
-      get :raffle
-      expect(response).to have_http_status(:success)
+  describe "PUT #update" do
+    before(:each) do 
+      @new_campaign_attributes = attributes_for(:campaign)
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
+
+    context "User is the campaign owner" do
+      before(:each) do 
+        campaign = create(:campaign, user: @current_user)
+        put :update, params: {id: campaign.id, campaign: @new_campaign_attributes}
+      end
+
+      it "return http success" do 
+        expect(response).to have_http_status(:success)
+      end
+
+      it "Campaign have new attributes" do 
+        expect(Campaign.last.title).to eql(@new_campaign_attributes[:title])
+        expect(Campaign.last.description).to eql(@new_campaign_attributes[:description])
+      end
+    end
+
+    context "User isn`t the campaign owner" do
+      it "return http forbidden" do 
+        campaign = create(:campaign)
+        put :update, params: {id: campaign.id, campaign: @new_campaign_attributes}
+        expect(response).to have_http_status(:forbidden)
+      end
     end
   end
 
+  describe "GET #raffle" do 
+    before(:each) do 
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
+
+    context "User is the campaign owner" do 
+      before(:each) do 
+        @campaign = create(campaign, user: @current_user)
+      end
+      
+      context "Has more than two members" do 
+        before(:each) do 
+          create(:member, campaign: @campaign)
+          create(:member, campaign: @campaign)
+          create(:member, campaign: @campaign)
+
+          post :raffle, params: {id: campaign.id}
+        end
+
+        it "returns http_success" do 
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context "hasn`t two members or more" do 
+        before(:each) do 
+          create(:member, campaign: @campaign)
+          post :raffle, params: {id: campaign.id}
+        end
+
+        it "return http unprocessable entity" do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+
+    context "user isn`t the campaing owner" do 
+      before(:each) do 
+        @campaign = create(:campaign)
+        post :raffle, params: {id: campaign.id}
+      end
+
+      it "returns http forbidden" do 
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
 end
