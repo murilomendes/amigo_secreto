cf2a6f70ebf48fe8640fb12cd13f17f9d7303ed8
Murilo Mendes <mmendes090@live.com>
2020-03-25 22:54:56 -0300

    set 404 redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   protect_from_forgery with: :exception
+
+  def render_404
+    redirect_to main_app.root_url
+  end
 end
