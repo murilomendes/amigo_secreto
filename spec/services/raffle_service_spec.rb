486f5cc4747794396f044ff37c660ef2469b6912
Murilo Mendes <mmendes090@live.com>
2020-03-25 22:23:46 -0300

    create raffle service engine

--- spec/services/raffle_service_spec.rb
+++ spec/services/raffle_service_spec.rb
@@ -6,52 +6,52 @@ describe RaffleService do
     @campaign = create(:campaign, status: :pending)
   end
 
-  describe "#call" do 
+  describe '#call' do
     context "when has more then two members" do
       before(:each) do
         create(:member, campaign: @campaign)
         create(:member, campaign: @campaign)
         create(:member, campaign: @campaign)
-
         @campaign.reload
 
         @results = RaffleService.new(@campaign).call
       end
 
-      it "returns a hash" do 
-        expect(@results.class).to eql(Hash)
+      it "results is a hash" do
+        expect(@results.class).to eq(Hash)
       end
 
-      it "all members are in results as member" do
+      it "all members are in results as a member" do
         result_members = @results.map {|r| r.first}
-        expect(result_members.sort).to eql(@campaign.members.sort)
+        expect(result_members.sort).to eq(@campaign.members.sort)
       end
 
-      it "all member are in results as a friend" do 
-        result_friends = @result.map {|r| r.last}
-        expect(result_friends.sort).to eql(@campaign.members.sort)
+      it "all member are in results as a friend" do
+        result_friends = @results.map {|r| r.last}
+        expect(result_friends.sort).to eq(@campaign.members.sort)
       end
 
-      it "a member don`t get yourself" do 
-        @result.each do |r| 
-          expect(r.first).not_to eql(r.last)
+      it "a member don't get yourself" do
+        @results.each do |r|
+          expect(r.first).not_to eq(r.last)
         end
       end
 
       it "a member x don't get a member y that get the member x" do
         # Desafio
       end
+
     end
 
-    context "when hasn`t more then two members" do 
+    context "when don't has more then two members" do
       before(:each) do
-        create(:member, campaing: @campaign)
+        create(:member, campaign: @campaign)
         @campaign.reload
 
         @response = RaffleService.new(@campaign).call
       end
 
-      it "raiser error" do
+      it "raise error" do
         expect(@response).to eql(false)
       end
     end
