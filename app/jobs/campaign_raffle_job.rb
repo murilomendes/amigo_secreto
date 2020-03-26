e1abdb5132c1da6384d4da28926fbff56b4ad23f
Murilo Mendes <mmendes090@live.com>
2020-03-25 22:49:59 -0300

    set raffle job

---
+++ app/jobs/campaign_raffle_job.rb
@@ -0,0 +1,15 @@
+class CampaignRaffleJob < ApplicationJob
+  queue_as :emails
+
+  def perform(campaign)
+    results = RaffleService.new(campaign).call
+
+    campaign.members.each { |m| do m.set_pixel }  
+
+    results.each do |r| 
+      CampaignMailer.raffle(campaign, r.first, r.last).deliver_now
+    end
+
+    campaign.update(status: :finished)
+  end
+end
