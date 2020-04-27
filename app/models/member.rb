8182b858a21156f15981be5d4224ba698bc45f01
Murilo Mendes <mmendes090@live.com>
2020-04-27 08:08:39 -0300

    Add Coffee to campaign

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,8 @@
 class Member < ApplicationRecord
   belongs_to :campaign
-  after_save :set_campaign_pending
+  after_destroy :set_campaign_pending
   validates :name, :email, :campaign, presence: true
-
+  
   def set_pixel
     self.open = false
     self.token = loop do
