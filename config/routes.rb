f705a728279eabd3da1977cd4f56228215de042e
Murilo Mendes <mmendes090@live.com>
2020-05-25 22:32:08 -0300

    config routes sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
   resources :members, only: [:create, :destroy, :update]
   get 'members/:token/opened', to: 'members#opened'
+
+  require 'sidekiq/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == 'mm' && password == 'mendes'
+  end
+  mount Sidekiq::Web => '/sidekiq'
 end
